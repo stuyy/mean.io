a112e455475f349a3b092f0ed4968ce93002b0fa
Ansonfoong <afoong@albany.edu>
2019-06-09 13:43:49 -0400

    Setting up s erver

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const auth = require('./routes/auth');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', auth);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log("http://localhost:" + PORT + "/"));
---
+++ server/routes/auth.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res) => {
+    res.send('ok');
+})
+module.exports = router;
