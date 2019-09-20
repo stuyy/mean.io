9f4b225c075e1fa0a9e71e21a7cbc2b609750dcf
Anson Foong <afoong@albany.edu>
2019-09-20 01:59:09 -0400

    Updated axios request to service request.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,43 +3,40 @@ import {  Article } from '../../models/Article';
 import { Router }  from '@angular/router';
 
 import axios from 'axios';
+import { AuthoritzationService } from 'src/app/services/authoritzation.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [ AuthoritzationService ]
 })
 export class DashboardComponent implements OnInit {
 
   articles: Article[];
-  constructor(private router: Router) { 
+  constructor(private router: Router, private authService: AuthoritzationService) { 
     this.router  = router;
   }
 
   ngOnInit() {
 
     // Need to fetch articles from the database.
-    axios.get('http://142.93.2.238:3000/article/get')
+    axios.get('http://localhost:3000/article/get')
     .then(res => {
       this.articles = res.data;
     })
     .catch(err => console.log(err));
 
+    this.authService.authorizeUser()
+    .then(res => console.log(res))
+    .catch(err => this.router.navigate(['/guest']))
     
-    axios.get('http://142.93.2.238:3000/isloggedin', { withCredentials: true})
-    .then(res  => {
-      console.log("User is authenticated.");
-    })
-    .catch(err => {
-      console.log("User is not authenticated.");
-      this.router.navigate(['/guest']);
-    });
   }
 
   logout($event)
   {
     $event.preventDefault();
-    axios.get('http://142.93.2.238:3000/logout', {withCredentials: true})
+    axios.get('http://localhost:3000/logout', {withCredentials: true})
     .then(res => {
       console.log(res);
       this.router.navigate(['/guest']);
